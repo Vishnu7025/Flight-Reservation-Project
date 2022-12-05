4f7d5cc84da1e74d11e09a238a0ae2e1ef23a31d
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-05 10:44:33 +0530

    Views

--- flightServices/flightApp/views.py
+++ flightServices/flightApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from flightApp.models import Flight,Passenger,Reservation
+from flightApp.serializers import FlightSerializer,PassengerSerializer,ReservationSerializer
+from rest_framework import viewsets
 # Create your views here.
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+class PassengerViewSet(viewsets.ModelViewSet):
+    queryset = Passenger.objects.all()
+    serializer_class = PassengerSerializer
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    serializer_class = ReservationSerializer
