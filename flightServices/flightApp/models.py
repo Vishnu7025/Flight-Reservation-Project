c29e89e89b7a6778fcc3e39ff607cc35af16f90a
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-07 21:14:00 +0530

    Automate Token Creation

--- flightServices/flightApp/models.py
+++ flightServices/flightApp/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals  import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
 
 # Create your models here.
 class Flight(models.Model):
@@ -23,3 +27,8 @@ class Passenger(models.Model):
 class Reservation(models.Model):
     flight = models.ForeignKey(Flight,on_delete=models.CASCADE)
     passenger = models.OneToOneField(Passenger,on_delete=models.CASCADE)
+
+@receiver(post_save,sender = settings.AUTH_USER_MODEL)
+def createAuthToken(sender,instance,created,**kwargs):
+    if created:
+        Token.objects.create(user=instance  )
