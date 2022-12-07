26529e12a2f8368503d1eff0bedd88eaf02a381a
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-07 11:29:20 +0530

    Custom validation

--- flightServices/flightApp/serializers.py
+++ flightServices/flightApp/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 from flightApp.models import Flight,Passenger,Reservation
+import re
 
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = '__all__'
+
+    def validate_flightNumber(self,flightNumber):
+        if(re.match("^[a-zA-Z0-9]*$",flightNumber) == None):
+            raise serializers.ValidationError("Invalid Flight  . Make sure it is alpha numeric")
+        return  flightNumber
+
 
 class PassengerSerializer(serializers.ModelSerializer):
     class Meta:
