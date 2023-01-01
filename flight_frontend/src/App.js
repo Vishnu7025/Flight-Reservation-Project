8522dceca6d384ae0ae05b09395213261d17d6b7
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-01 10:55:51 +0530

    starting front end app

---
+++ flight_frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>heloo</h2>
+       
+    </div>
+  );
+}
+
+export default App;
