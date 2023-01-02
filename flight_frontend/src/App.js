d8b8b6787b4a26bebcc4e94005c2adf5078a4019
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-02 12:06:49 +0530

    add components

--- flight_frontend/src/App.js
+++ flight_frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginServices from './components/LoginServices';
 
 function App() {
   return (
     <div className="App">
       <h2>heloo</h2>
+      < LoginServices/>
        
     </div>
   );
---
+++ flight_frontend/src/components/LoginServices.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function LoginServices() {
+  return (
+    <div>
+        <h1>hello login</h1>
+    </div>
+  )
+}
+
+export default LoginServices
